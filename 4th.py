0903986c80ced8c65b38a0d40ec7d713b2bacd37
Ranganath Gowda <hemanthranganathgowda@gmail.com>
2026-03-25 22:29:31 +0530

    updated code

--- 4th.py
+++ 4th.py
@@ -1,34 +1,9 @@
-"""i=1
-while i<=10:
-    print(i)
-    i+=1"""
-
-
-"""for i in range(10,0,-1):
-    print(i)"""
 """total=0
-for i in range(1,51):
+for i in range(1,101):
     total+=i
     print(total)"""
 
-
-"""num=5
-for i in range(1,11):
-    print(num,"*",i,"=",num*i)"""
-#for i in range(1,11):
- #   print(i*i*i)
-"""for i in range(1,51):
-    if i%3==0:
-        print(i) """
-"""total=0
-for i in range(1,100):
-    if i%2==0:
-        total+=i
-        print(total)"""
-"""for i in range(1,11):
-    print(i*7)"""
-
-"""num=int(input("enter the digits:"))
+"""num=int(input("enter the number:"))
 temp=num
 rev=0
 while num>0:
@@ -36,47 +11,88 @@ while num>0:
     rev=rev*10+digit
     num=num//10
 if temp==rev:
-    print("The given no is palindromic")
+    print("palindromic")
 else:
-    print("the given number is not palindromic")"""
+    print("not a palindromic")"""
 
 
-'''for i in range(1,101):
-    if i==50:
+"""num=int(input("enter a number :"))
+if num>1:
+    for i in range(2,num):
+        if num%i==0:
+            print("not a prime number")
+    else:
+        print("prime number")
+else:
+    print("not a prime number")"""
+
+
+"""print(15%3)
+print(17%5)"""
+"""n=20
+if n%4==0:
+    print("yes divisible by 4")"""
+
+"""num=int(input("enter a number:"))
+if num>1:
+    for i in range(2,num):
+        if num%i==0:
+            print("not a prime number")
+            break
+        else:
+            print("yes it is a prime number")
+            break"""
+
+
+
+
+"""amount=int(input("enter the total amount:"))
+if amount%100!=0:
+    print("invalid balance")
+else:    
+    n2000=amount//2000
+    amount=amount%2000
+    n500=amount//500
+    amount=amount%500
+    n100=amount//100
+
+print("500notes",n500)
+print("2000notes",n2000)
+print("n100notes",n100)"""
+
+
+"""total=0
+count=0
+while True:
+    amount=int(input("enter the amount:"))
+    if amount==0:
         break
-        print(i)'''
-"""for i in range(1,101):
-    if i%3==0:
-        continue
-    print(i)"""
-"""while True:
-    express=int(input("enter an number:"))
-    if express==0:
-        break
-    print(express)"""
+    total+=amount
+    count+=1
+print('total amount:',total)
+print("number of depositers",count)"""
 
-"""for i in range(1,5):
-    for j in range(i):
-        print("*",end=" ")
-    print()"""
+"""num=int(input("enter the number:"))
+temp=num
+sum=0
+while num>0:
+    digit=num%10 
+    sum=sum+digit**3
+    num=num//10
+if sum==temp:
+    print("Armstrong number:")
+else:
+    print("not an armstrong number")"""
 
-for i in range(4,0,-1):
-    for j in range(i):
-        print("*",end=" ")
-    print()
-    
-        
+n=int(input("enter the limit:"))
+for num in range(1,n+1):
+    original =num
+    temp=num
+    sum=0
+    while temp>0:
+        digit=temp%10
+        sum=sum+digit**3
+        temp=temp//10
+    if sum==original:
+        print("Armstrong number",original)
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
