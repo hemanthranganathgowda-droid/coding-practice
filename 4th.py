979af8417413664a73d91989158b3d08296d4dee
Ranganath Gowda <hemanthranganathgowda@gmail.com>
2026-03-25 17:51:07 +0530

    updated new file

---
+++ 4th.py
@@ -0,0 +1,82 @@
+"""i=1
+while i<=10:
+    print(i)
+    i+=1"""
+
+
+"""for i in range(10,0,-1):
+    print(i)"""
+"""total=0
+for i in range(1,51):
+    total+=i
+    print(total)"""
+
+
+"""num=5
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)"""
+#for i in range(1,11):
+ #   print(i*i*i)
+"""for i in range(1,51):
+    if i%3==0:
+        print(i) """
+"""total=0
+for i in range(1,100):
+    if i%2==0:
+        total+=i
+        print(total)"""
+"""for i in range(1,11):
+    print(i*7)"""
+
+"""num=int(input("enter the digits:"))
+temp=num
+rev=0
+while num>0:
+    digit=num%10
+    rev=rev*10+digit
+    num=num//10
+if temp==rev:
+    print("The given no is palindromic")
+else:
+    print("the given number is not palindromic")"""
+
+
+'''for i in range(1,101):
+    if i==50:
+        break
+        print(i)'''
+"""for i in range(1,101):
+    if i%3==0:
+        continue
+    print(i)"""
+"""while True:
+    express=int(input("enter an number:"))
+    if express==0:
+        break
+    print(express)"""
+
+"""for i in range(1,5):
+    for j in range(i):
+        print("*",end=" ")
+    print()"""
+
+for i in range(4,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
