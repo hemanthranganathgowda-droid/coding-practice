8aa0f0b3eed26bd06c55fc0ab995e436a3892602
Ranganath Gowda <hemanthranganathgowda@gmail.com>
2026-04-07 19:18:45 +0530

    updated

--- app1.py
+++ app1.py
@@ -1,56 +1,26 @@
-"""from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def home():
-    return "Ranga is fucking Muttu"
-app.run(debug=True)"""
-
-"""from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def home():
-    return "<h1>Ranga is loving a girl name called nithya</h1>"
-app.run(debug=True)"""
-
-"""from flask import Flask, request, jsonify
+"""from flask import Flask,request,jsonify
 app=Flask(__name__)
 @app.route('/user')
-def user():
-    name= request.args.get('name')
-    return f"<h1>Hello {name} </h1>"
-@app.route('/add',methods=['POST'])
-def add ():
-    data=request.get_json()
-    if not data:
-        return jsonify({"error": "No data recieved"}), 400
-    return jsonify(data)
+
+    
+@app.route('/login',methods=['post'])
+def login():
+    data=request.json
+    if data['user_name']=='Hemanth' and data['passward']=='1234':
+        return jsonify({'message':'login sucessfully'})
+    else:
+        return jsonify({'message':'invalid user name'})
 app.run(debug=True)"""
 
-from flask import Flask, request, jsonify
-import sqlite3
 
-app = Flask(__name__)
-
-# connect DB
-conn = sqlite3.connect('users.db', check_same_thread=False)
-cursor = conn.cursor()
-
-# create table
-cursor.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)")
-
-# add user
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.get_json()
-    cursor.execute("INSERT INTO users VALUES (?, ?)", (data['name'], data['age']))
-    conn.commit()
-    return jsonify({"message": "saved"})
-
-# get users
-@app.route('/get_users')
-def get_users():
-    cursor.execute("SELECT * FROM users")
-    users = cursor.fetchall()
-    return jsonify(users)
-
+from flask import Flask,request
+app=Flask(__name__)
+@app.route('/login')
+def login():
+    username=request.args.get('username')
+    passward=request.args.get('passward')
+    if username=='hemanth' and passward=='1234':
+        return "login successfully"
+    else:
+        return "invalid user name"
 app.run(debug=True)
