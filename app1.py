ae3e920e9081307d4d5e51f1c849fbea44a5f486
Ranganath Gowda <hemanthranganathgowda@gmail.com>
2026-04-08 20:58:43 +0530

    update

--- app1.py
+++ app1.py
@@ -29,7 +29,7 @@ from flask import Flask,request
 app=Flask(__name__)
 users=[
     {'username':'hemanth','password':'1234'},
-    {'username':'ravi',}
+    {'username':'ravi','password':'5678'}
 ]
 @app.route('/login',methods=['POST'])
 def logic():
